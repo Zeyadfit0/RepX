c630c72bf541da4a6d393bd1f2014d8a041510f5
Zeyadfit0 <azyad2755@gmail.com>
2025-07-15 22:50:06 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,13 @@
-const CACHE_NAME = "repx-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./logo.png.png",
-  "./manifest.json"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', function(event) {
+  caches.keys().then(function(cacheNames) {
+    return Promise.all(
+      cacheNames.map(function(cacheName) {
+        return caches.delete(cacheName); // 🧹 امسح كل الكاش
+      })
+    );
+  });
 });
